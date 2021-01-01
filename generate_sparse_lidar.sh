d886f58f0b4ea4de84d1e7df661141423704ac04
dogyoonlee <nemotio@yonsei.ac.kr>
2021-01-02 00:50:14 +0900

    multi lidar sampling update

---
+++ generate_sparse_lidar.sh
@@ -0,0 +1,8 @@
+# python kitti_sparsify.py --slice 4 --data_mode training
+# python kitti_sparsify.py --slice 4 --data_mode testing
+# python kitti_sparsify.py --slice 8 --data_mode training
+# python kitti_sparsify.py --slice 8 --data_mode testing
+# python kitti_sparsify.py --slice 16 --data_mode training
+# python kitti_sparsify.py --slice 16 --data_mode testing
+python kitti_sparsify.py --slice 32 --data_mode training
+python kitti_sparsify.py --slice 32 --data_mode testing
---
+++ generate_sparse_multi_lidar.sh
@@ -0,0 +1,10 @@
+# python kitti_sparsify.py --slice 2 --data_mode training --multi_lidar
+# python kitti_sparsify.py --slice 2 --data_mode testing --multi_lidar
+# python kitti_sparsify.py --slice 4 --data_mode training --multi_lidar
+# python kitti_sparsify.py --slice 4 --data_mode testing --multi_lidar
+# python kitti_sparsify.py --slice 8 --data_mode training --multi_lidar
+python kitti_sparsify.py --slice 8 --data_mode testing --multi_lidar
+python kitti_sparsify.py --slice 16 --data_mode training --multi_lidar
+python kitti_sparsify.py --slice 16 --data_mode testing --multi_lidar
+python kitti_sparsify.py --slice 32 --data_mode training --multi_lidar
+python kitti_sparsify.py --slice 32 --data_mode testing --multi_lidar
