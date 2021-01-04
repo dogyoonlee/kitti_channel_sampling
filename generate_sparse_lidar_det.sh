7326d4b1877d8966c20f52504ebeaad261822896
dogyoonlee <nemotio@yonsei.ac.kr>
2021-01-04 14:05:54 +0900

    det sh file

---
+++ generate_sparse_lidar_det.sh
@@ -0,0 +1,11 @@
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 2
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 4
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 8
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 16
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 32
+
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 2 --multi_lidar
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 4 --multi_lidar
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 8 --multi_lidar
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 16 --multi_lidar
+python kitti_sparsify_detection.py --data_dir ./data_det --multi_lidar --det --slice 32 --multi_lidar
