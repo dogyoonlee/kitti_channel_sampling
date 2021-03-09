24c726d7743f5a1dd5155849656db529deda6055
Dogyoon Lee <nemotio@yonsei.ac.kr>
2021-03-09 13:22:00 +0900

    sampling3 update

---
+++ generate_sparse_multi_lidar_det_sampling3.sh
@@ -0,0 +1,6 @@
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling3 --multi_lidar --sampling_num3 --slice 2
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling3 --multi_lidar --sampling_num3 --slice 4
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling3 --multi_lidar --sampling_num3 --slice 8
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling3 --multi_lidar --sampling_num3 --slice 16
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling3 --multi_lidar --sampling_num3 --slice 32
+
