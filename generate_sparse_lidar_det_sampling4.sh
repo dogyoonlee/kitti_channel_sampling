3714ff0bdbde9420e82160f10a8f5755c90e1433
dogyoonlee <nemotio@yonsei.ac.kr>
2021-03-22 13:40:36 +0900

    sampling4 shell update

--- generate_sparse_lidar_det_sampling4.sh
+++ generate_sparse_lidar_det_sampling4.sh
@@ -1,6 +1,6 @@
 
-python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --multi_ratio 10 --slice 2
-python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --multi_ratio 10 --slice 4
-python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --multi_ratio 10 --slice 8
-python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --multi_ratio 10 --slice 16
-python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --multi_ratio 10 --slice 32
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --sampling_num4 --multi_ratio 10 --slice 2
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --sampling_num4 --multi_ratio 10 --slice 4
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --sampling_num4 --multi_ratio 10 --slice 8
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --sampling_num4 --multi_ratio 10 --slice 16
+python kitti_sparsify_detection.py --data_dir ./data_det --det --sparse_lidar_save_path ./sparse_lidar_results_det/sparse_lidar_det_sampling4 --sampling_num4 --multi_ratio 10 --slice 32
